a96b5d68a170d57331b4155c9260b8f5562836ea
Andrew <andrew.cuddon@gmail.com>
2015-03-10 21:03:06 +1100

    File transport options

--- logger.js
+++ logger.js
@@ -4,21 +4,40 @@
 
 // Winston logging library
 var winston = require('winston');
-require('winston-mongo').Mongo;
+//require('winston-mongo').Mongo;
 
 // Settings
-var logfile = './request.log';
-var exceptionsfile = './exceptions.log';
+var requestlog = ""./request.log";
+var errorlog = "./error.log";
+var exceptionsfile = "./exceptions.log";
 
 // Set up logger and save locations
 var logger = new (winston.Logger)({
   transports: [
-    new winston.transports.Console()
-    new winston.transports.File({filename: logfile})
+    new winston.transports.Console({
+      colorize: true
+    }),
+    
+    new winston.transports.File({
+      name: "request-log-file",
+      filename: requestlog,
+      timestamp: true,
+      json: true,
+      maxsize: 1000000,
+      maxFiles: 1
+    },
+    
+    new winston.transports.File({
+      name: "error-log-file",
+      filename: errorlog,
+      level: 'error',
+      timestamp: true,
+      json: true,
+      maxsize: 1000000,
+      maxFiles: 1,
+      handleExceptions: true
+    })
     //new winston.transports.MongoDB({ db: 'db', level: 'info'})
-  ]
-  exceptionHandlers: [
-    new winston.transports.File({ filename: exceptionsfile })
   ]
 });
 
@@ -29,3 +48,27 @@ function getlogs(level) {
   
   return logs;
 }
+
+
+// Tests
+logger.info('Hello distributed logs. This is FYI only');
+logger.warn('This is a warning');
+logger.error('This is an error with some data', , {some: 'data''});
+
+
+// Find items logged between today and yesterday.
+var options = {
+    from: new Date - 24 * 60 * 60 * 1000,
+    until: new Date,
+    limit: 10,
+    start: 0,
+    order: 'desc',
+    fields: ['message']
+  };
+
+  winston.query(options, function (err, results) {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+  });
